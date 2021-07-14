04ea70e5e9f6d635bcefb87e3334b166ddc91efd
Angelique Grincourt <angelique.grnt@gmail.com>
2021-07-14 10:19:45 +0200

    feat: loginscreen

--- groupomaniaFront/src/app/auth/auth.component.ts
+++ groupomaniaFront/src/app/auth/auth.component.ts
@@ -4,23 +4,21 @@ import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
-
   email = new FormControl('', [Validators.required, Validators.email]);
   hide = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor() {}
 
   getErrorMessage() {
     if (this.email.hasError('required')) {
-      return 'Merci de remplir ce champ';
+      return 'You must enter a value';
     }
 
-    return this.email.hasError('email') ? 'email non valide' : '';
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
+
+  ngOnInit(): void {}
 }
